fea6eaed80003b19aab45769025f46c415a5dfea
Dzhin2001 <Dzhin2001@mail.ru>
2022-10-16 22:26:16 +0300

    controller

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,22 +1,41 @@
 import 'reflect-metadata';
 import {inject, injectable} from 'inversify';
+import express, {Express} from 'express';
 import {LoggerInterface} from '../common/logger/logger.interface.js';
 import {ConfigInterface} from '../common/config/config.interface.js';
 import {Component} from '../types/component.types.js';
 import {getURI} from '../utils/db.js';
 import {DatabaseInterface} from '../common/database-client/database.interface.js';
-import { UserServiceInterface } from '../modules/user/user-service.interface.js';
-import { FilmServiceInterface } from '../modules/film/film-service.interface.js';
+import {ControllerInterface} from '../common/controller/controller.interface.js';
+import {ExceptionFilterInterface} from '../common/errors/exception-filter.interface.js';
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
     @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface,
-    @inject(Component.UserServiceInterface) private userService: UserServiceInterface,
-    @inject(Component.FilmServiceInterface) private filmService: FilmServiceInterface
-  ) {}
+    @inject(Component.UserController) private userController: ControllerInterface,
+    @inject(Component.FilmController) private filmController: ControllerInterface,
+    @inject(Component.ExceptionFilterInterface) private exceptionFilter: ExceptionFilterInterface,
+  ) {
+    this.expressApp = express();
+  }
+
+  public initRoutes() {
+    this.expressApp.use('/users', this.userController.router);
+    this.expressApp.use('/films', this.filmController.router);
+  }
+
+  public initMiddleware() {
+    this.expressApp.use(express.json());
+  }
+
+  public initExceptionFilters() {
+    this.expressApp.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+  }
 
   public async init() {
     this.logger.info('Application initialization...');
@@ -32,10 +51,10 @@ export default class Application {
 
     await this.databaseClient.connect(uri);
 
-    const user = await this.userService.findByEmail('freedom@pisem.local');
-    console.log(user);
-
-    const film = await this.filmService.findPromo();
-    console.log(film);
+    this.initMiddleware();
+    this.initRoutes();
+    this.initExceptionFilters();
+    this.expressApp.listen(this.config.get('PORT'));
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
